9f746720a9cb7b573b8870946490e817cc9b9b69
Arsenii Karpov <abs2016123@gmail.com>
2025-07-04 16:59:55 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,76 @@
+import asyncio
+import logging
+import os.path
+import sys
+import time
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message, FSInputFile
+from aiogram import F
+
+from dotenv import load_dotenv
+
+import downloader
+
+load_dotenv()
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = getenv("BOT_TOKEN")
+
+YOUTUBE_REGEX = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?$"
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message(F.text.regexp(YOUTUBE_REGEX))
+async def download_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        path = await asyncio.to_thread(downloader.download, message.text)
+        if os.path.isfile(path):
+            video = FSInputFile(path)
+            await message.answer_video(video)
+        else:
+            print("AAAA")
+
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
---
+++ downloader.py
@@ -0,0 +1,59 @@
+import json
+import os
+import datetime as dt
+
+from yt_dlp import YoutubeDL
+
+OUTPUT_VIDEO_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "videos")
+if not os.path.exists(OUTPUT_VIDEO_FOLDER):
+    os.mkdir(OUTPUT_VIDEO_FOLDER)
+
+YT_DLP_CMD = ('yt-dlp -f "bv[ext=mp4][vcodec^=avc]+ba[ext=m4a]/b[ext=mp4]" --merge-output-format mp4 %s -P '
+              + f'"{OUTPUT_VIDEO_FOLDER}" -o "hhghg.%(ext)s"')
+
+
+def generate_unique_filename(prefix, suffix):
+    now = dt.datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S_%f")
+    return f"{prefix}{timestamp}{suffix}"
+
+
+def download(url):
+    audio_filename = generate_unique_filename("audio_", ".m4a")
+    video_filename = generate_unique_filename("video_", ".mp4")
+    result_filename = generate_unique_filename("result_", ".mp4")
+
+    # try:
+    #     opts = {
+    #         'extract_flat': 'discard_in_playlist',
+    #         'format': 'bv*[ext=mp4][vcodec^=avc]]',
+    #         'fragment_retries': 10,
+    #         'ignoreerrors': 'only_download',
+    #         'merge_output_format': 'mp4',
+    #         'outtmpl': {'default': video_filename},
+    #         'paths': {'home': OUTPUT_VIDEO_FOLDER},
+    #         'postprocessors': [{'key': 'FFmpegConcat',
+    #                             'only_multi_video': True,
+    #                             'when': 'playlist'}],
+    #         'retries': 10
+    #     }
+    #     with YoutubeDL(opts) as ydl:
+    #         ydl.download(url)
+    #
+    #     if not os.path.exists(video_filename)
+    opts = {
+        'extract_flat': 'discard_in_playlist',
+        'format': 'bv[ext=mp4][vcodec^=avc]+ba[ext=m4a]/b[ext=mp4]',
+        'fragment_retries': 10,
+        'ignoreerrors': 'only_download',
+        'merge_output_format': 'mp4',
+        'outtmpl': {'default': video_filename},
+        'paths': {'home': OUTPUT_VIDEO_FOLDER},
+        'postprocessors': [{'key': 'FFmpegConcat',
+                            'only_multi_video': True,
+                            'when': 'playlist'}],
+        'retries': 10
+    }
+    with YoutubeDL(opts) as ydl:
+        ydl.download([url])
+    return os.path.join(OUTPUT_VIDEO_FOLDER, video_filename)
